1792c8cf30fc8dedca1c97bb77e6477dfcf691a7
Alex <pointalexandre@gmail.com>
2017-10-08 10:55:49 +0200

    Write output files on the fly to avoid OutOfMemoryExceptions

--- kali-xml-parser/Program.cs
+++ kali-xml-parser/Program.cs
@@ -18,43 +18,88 @@ namespace kali_xml_parser
             var inputDirectory = applicationPath + "input\\";
             var outputDirectory = applicationPath + "output\\";
 
-            var rootElt = new XElement("root");
-            
+            // Cleanup output directory
+            var di = new DirectoryInfo(outputDirectory);
+            foreach (var file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (var dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
             // Browse all xml files and append their content to the root element
+            var idToNodes = new Dictionary<string, List<XElement>>();
             foreach (var file in Directory.GetFiles(inputDirectory, "*.xml", SearchOption.AllDirectories))
             {
                 var doc = XDocument.Load(file);
-                rootElt.Add(doc.Root);
-            }
 
-            // 
-            var bundleDoc = new XDocument(rootElt);
-
-            // Save the compiled xml file
-            var outputFile = outputDirectory + "bundle.xml";
-            if (!Directory.Exists(outputDirectory))
-            {
-                Directory.CreateDirectory(outputDirectory);
-            }
-            bundleDoc.Save(outputFile);
-            Console.WriteLine("Bundle xml file written at {0}", new Uri(outputFile).AbsolutePath);
-
-            // Cleanup unescaped tags (p, sup, em, font, br)
-            var tagsToEscapeRegex = new Regex(@"<(p|sup|em|font|br)([^>]*)>");
-            var cleanOutputFile = outputDirectory + "bundle-clean.xml";
-            using (var input = File.OpenText(outputFile))
-            using (var output = new StreamWriter(cleanOutputFile))
-            {
-                string line;
-                while (null != (line = input.ReadLine()))
+                var collAgreeNumber = doc.Descendants("CONTENEUR").Attributes().FirstOrDefault(att => att.Name == "num");
+                if (collAgreeNumber != null)
                 {
-                    // optionally modify line.
-                    var newLine = tagsToEscapeRegex.Replace(line, "<$1$2>");
-                    output.WriteLine(newLine);
-                }
-            }
-            Console.WriteLine("Cleanup bundle xml file written at {0}", new Uri(cleanOutputFile).AbsolutePath);
+                    //Console.WriteLine("Found file with collective agreement: {0}", file);
+                    var id = collAgreeNumber.Value;
+                    if (!idToNodes.ContainsKey(id))
+                    {
+                        // Write previous collective agreement and flush dictionary
+                        // write all documents (one per collective agreement)
+                        foreach (var entry in idToNodes)
+                        {
+                            var outputFile = outputDirectory + entry.Key + ".xml";
 
+                            var bundleDoc = new XDocument(new XElement("root"));
+                            if (File.Exists(outputFile))
+                            {
+                                bundleDoc = XDocument.Load(outputFile);
+                            }
+
+                            foreach (var node in entry.Value)
+                            {
+                                bundleDoc.Root.Add(node);
+                            }
+
+                            // Save the compiled xml file
+                            
+                            if (!Directory.Exists(outputDirectory))
+                            {
+                                Directory.CreateDirectory(outputDirectory);
+                            }
+                            bundleDoc.Save(outputFile);
+                            Console.WriteLine("Bundle xml file written at {0}", new Uri(outputFile).AbsolutePath);
+
+
+                            // Cleanup unescaped tags (p, sup, em, font, br)
+                            var tagsToEscapeRegex = new Regex(@"<(/)?(p|sup|em|font|br)([^>]*)>");
+                            var cleanOutputFile = outputDirectory + entry.Key + "-clean.xml";
+                            using (var input = File.OpenText(outputFile))
+                            using (var output = new StreamWriter(cleanOutputFile))
+                            {
+                                string line;
+                                while (null != (line = input.ReadLine()))
+                                {
+                                    // optionally modify line.
+                                    var newLine = tagsToEscapeRegex.Replace(line, "<$1$2$3>");
+                                    output.WriteLine(newLine);
+                                }
+                            }
+                            Console.WriteLine("Cleanup bundle xml file written at {0}", new Uri(cleanOutputFile).AbsolutePath);
+                        }
+
+                        idToNodes.Clear();
+
+                        idToNodes.Add(id, new List<XElement>());
+                    }
+                    idToNodes[id].Add(doc.Root);
+                }
+
+                // do nothing
+            }
+            //Console.WriteLine("Found {0} collective agreement ids", idToNodes.Count);
+
+            
+
+            Console.WriteLine("END");
             Console.ReadKey();
         }
     }
